b89d19ae09a538c1aee8f22ab84404dc94844d01
j. jillian <judith.mediosdigitales@gmail.com>
2022-05-17 12:00:20 +0200

    Tuesday Changes

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-let images = [
-    "url('./Images/background-1.jpg')",
-    "url('./Images/background-2.jpg')",
-    "url('./Images/background-3.jpg')",
-    "url('./Images/background-4.jpg')",
-    "url('./Images/background-5.jpg')",
-    "url('./Images/background-6.jpg')",
-    "url('./Images/background-7.jpg')",
-    "url('./Images/background-8.jpg')"
-];
+const cards = document.querySelectorAll('.memory-card');
 
-let section = document.querySelector('#sectionId');
+function flipCard(){
+    this.classList.toggle('flip');
+     // console.log('i was clicked');
+    // console.log(this);
+}
 
 
-
-setInterval(function () {
-    let bg = images[Math.floor(Math.random()* images.length)]
-    section.style.backgroundImage = bg;
-    console.log("hello");
-    
-},1000);
-
-
-document.getElementById("myBtn").addEventListener("click", function() {
-    alert("Hello World!");
-    console.log("Btn work's");
-  });
+cards.forEach(card => card.addEventListener ('click', flipCard));
